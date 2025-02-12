9d0b18667a3a590a4cc365bab3f4b16bec29fb7a
Ozzy-ZY <Zyadhimself1@gmail.com>
2025-02-12 21:41:06 +0200

    added a test Controller to test the auth

---
+++ API/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TestController:ControllerBase
+{
+    [HttpGet("OnlyAuthUsers")]
+    [Authorize]
+    public IActionResult OnlyAuthUsers()
+    {
+        return Ok("You are logged in");
+    }
+
+    [HttpGet("OnlyAdminUsers")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult OnlyAdminUsers()
+    {
+        return Ok("You are logged in as Admin");
+    }
+}
