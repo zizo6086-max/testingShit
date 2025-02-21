cf294f024c49627925c3677d01181a85559f29b7
Ozzy-ZY <Zyadhimself1@gmail.com>
2025-02-22 00:56:29 +0200

    a User Service and Controller that Consumes the PhotoService and Enables multiple Operations

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Claims;
 using Application.DTOs;
+using Application.Services;
 using Domain.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -8,22 +9,52 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 [ApiController]
 [Route("[controller]")]
-public class UserController(UserManager<AppUser> userManager): ControllerBase
+public class UserController(UserService userService, UserManager<AppUser> userManager,PhotoService photoService,ILogger<UserController> logger): ControllerBase
 {
-    [HttpPost("ChangePassword")]
-    [Authorize(Roles = "User")]
+    [HttpGet("Me")]
+    [Authorize]
+    public async Task<IActionResult> GetMe()
+    {
+        var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value!);
+        var result = await userService.GetUserInfoAsync(userId);
+        return Ok(result);
+    }
+    [HttpPut("ChangePassword")]
+    [Authorize]
     public async Task<IActionResult> ChangePassword(ChangePasswordDto dto)
     {
-        var user = await userManager.FindByIdAsync(
-            User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value);
-        if (user == null)
-            return BadRequest(new {Message = "Error Getting User To change the Password"});
-        var result = await userManager.ChangePasswordAsync(user,dto.OldPassword,dto.NewPassword);
-        if (result.Succeeded)
+        var userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+        var result = await userService.ChangePasswordAsync(userId, dto);
+        if (result.Success)
         {
-            return Ok();
+            return Ok(result);
         }
-        return BadRequest(result.Errors);
+        return BadRequest(result);
     }
-    
+
+    [HttpPut("UpdateProfilePicture")]
+    [Authorize]
+    public async Task<IActionResult> UpdateProfilePicture(IFormFile file)
+    {
+        var userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value!;
+        var result = await userService.UpdateProfilePictureAsync(userId, file);
+        if (result.Success)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+
+    [HttpDelete("DeleteProfilePicture")]
+    [Authorize]
+    public async Task<IActionResult> DeleteProfilePicture()
+    {
+        var userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value!;
+        var result = await userService.DeleteProfilePictureAsync(userId);
+        if (result.Success)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
 }
---
+++ Application/Services/UserService.cs
@@ -0,0 +1,167 @@
+﻿using Application.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Services;
+
+public class UserService(UserManager<AppUser> userManager,
+    ILogger<UserService> logger,
+    PhotoService photoService)
+{
+    public async Task<UserDto> GetUserInfoAsync(int userId)
+    {
+        var user = await userManager.FindByIdAsync(userId.ToString());
+        if (user == null)
+        {
+            return new UserDto();
+        }
+
+        var result = new UserDto()
+        {
+            UserName = user.UserName,
+            Email = user.Email,
+            ImageUrl = user.ImageUrl,
+            IsVerified = user.EmailConfirmed
+        };
+        return result;
+    }
+    public async Task<Result> UpdateProfilePictureAsync(string userId, IFormFile file)
+    {
+        try
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return new Result()
+                {
+                    Message = "User does not exist"
+                };
+            }
+
+            if (!string.IsNullOrEmpty(user.ImageUrl))
+            {
+                await photoService.DeletePhotoAsync(user.ImageUrl);
+            }
+
+            var (fileUrls, errors) = await photoService.SavePhotosAsync(new[] { file }, "Users");
+            if (errors.Count > 0)
+            {
+                return new Result()
+                {
+                    Message = string.Join('\n', errors)
+                };
+            }
+
+            user.ImageUrl = fileUrls[0];
+            var updateResult = await userManager.UpdateAsync(user);
+            if (!updateResult.Succeeded)
+            {
+                await photoService.DeletePhotoAsync(user.ImageUrl);
+                return new Result()
+                {
+                    Message = "User could not be updated"
+                };
+            }
+
+            return new Result()
+            {
+                Success = true,
+                Message = "User Image updated",
+                Data = user.ImageUrl
+            };
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, $"Error Updating user Image for {userId}");
+            return new Result()
+            {
+                Message = "UnExpected Error Occurred Updating User Image "
+            };
+        }
+    }
+
+    public async Task<Result> DeleteProfilePictureAsync(string userId)
+    {
+        try
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return new Result()
+                {
+                    Message = "User does not exist"
+                };
+            }
+
+            if (string.IsNullOrEmpty(user.ImageUrl))
+            {
+                return new Result()
+                {
+                    Success = true,
+                    Message = "No Profile Picture available"
+                };
+            }
+
+            var deleteResult = await photoService.DeletePhotoAsync(user.ImageUrl);
+            if (!deleteResult)
+            {
+                return new Result()
+                {
+                    Message = "Failed to delete profile picture"
+                };
+            }
+
+            user.ImageUrl = "";
+            if (!(await userManager.UpdateAsync(user)).Succeeded)
+            {
+                return new Result()
+                {
+                    Message = "Failed to Update profile picture after Deleting it" // Improve later
+
+                };
+            }
+
+            return new Result()
+            {
+                Success = true,
+                Message = "Profile Picture deleted"
+            };
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, $"Error Deleting profile picture for {userId}");
+            return new Result()
+            {
+                Message = "UnExpected Error Occurred Deleting profile picture"
+            };
+        }
+    }
+
+    public async Task<Result> ChangePasswordAsync(string userId, ChangePasswordDto changePasswordDto)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            return new Result()
+            {
+                Message = "User does not exist"
+            };
+        }
+        var result = await userManager.ChangePasswordAsync(user, changePasswordDto.OldPassword, changePasswordDto.NewPassword);
+        if (!result.Succeeded)
+        {
+            return new Result()
+            {
+                Message = "Failed to change password",
+                Data = result.Errors
+            };
+        }
+        return new Result()
+        {
+            Success = true,
+            Message = "Changed Password"
+        };
+    }
+}
