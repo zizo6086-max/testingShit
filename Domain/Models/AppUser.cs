7ffdcae719ab6925c2939535dbe899e29609c5ed
Ozzy-ZY <Zyadhimself1@gmail.com>
2025-03-14 01:49:26 +0200

    changed the appuser model to add google auth feature

--- Domain/Models/AppUser.cs
+++ Domain/Models/AppUser.cs
@@ -8,4 +8,8 @@ public class AppUser:IdentityUser<int>
     public DateTimeOffset CreatedOn { get; set; } = DateTimeOffset.UtcNow;
     public string ImageUrl { get; set; }
     public ulong RowVersion { get; set; }
+    
+    // Properties for external authentication
+    public string? GoogleId { get; set; }
+    public bool IsExternalAccount { get; set; } = false;
 }
