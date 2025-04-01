776eeb6806f3ad08723a86b18bd9450f9bead66b
Ozzy-ZY <Zyadhimself1@gmail.com>
2025-04-01 03:08:20 +0200

    use of relative path to find the appsettings.json

--- Infrastructure/DataAccess/AppDbContextFactory.cs
+++ Infrastructure/DataAccess/AppDbContextFactory.cs
@@ -9,7 +9,7 @@ public class AppDbContextFactory:IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var config = new ConfigurationBuilder()
-            .SetBasePath("C:\\Users\\user\\source\\repos\\Creatsy\\API\\")
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(),"../API"))
             .AddJsonFile("appsettings.json")
             .Build();
 
