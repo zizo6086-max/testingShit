6850393eec17eb2f650576cbd133b57365831c8b
OZZY <Zyadhimself1@gmail.com>
2025-04-26 19:46:27 +0300

    Fix: removed old seeding code that is now deprecated with ef core 9

--- Domain/ModelsConfig/AppUserConfig.cs
+++ Domain/ModelsConfig/AppUserConfig.cs
@@ -12,31 +12,5 @@ public class AppUserConfig:IEntityTypeConfiguration<AppUser>
         builder.Property(u => u.UserName).IsRequired().HasMaxLength(64);
         builder.Property(u => u.RowVersion).IsRowVersion().HasConversion<byte[]>();
         builder.Property(u=> u.ImageUrl).IsRequired(false);
-        var hasher = new PasswordHasher<AppUser>();
-        var admin = new AppUser()
-        {
-            Id = 1,
-            UserName = "admin",
-            NormalizedUserName = "ADMIN",
-            Email = "zyadhimself1@gmail.com",
-            NormalizedEmail = "ZYADHIMSELF1@GMAIL.COM",
-            EmailConfirmed = true,
-            SecurityStamp = Guid.NewGuid().ToString(),
-        };
-        admin.PasswordHash = hasher.HashPassword(admin, "123@Admin");
-        var user = new AppUser()
-        {
-            Id = 2,
-            UserName = "user",
-            NormalizedUserName = "USER",
-            Email = "zyadhimself1@gmail.com",
-            NormalizedEmail = "ZYADHIMSELF1@GMAIL.COM",
-            EmailConfirmed = true,
-            SecurityStamp = Guid.NewGuid().ToString(),
-        };
-        user.PasswordHash = hasher.HashPassword(user, "123@Admin");
-        
-        builder.HasData(admin,user);
-        
     }
 }
