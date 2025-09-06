69c48196d00828eb208828b0984380c284fb43a2
Ozzy-ZY <zyadhimself1@gmail.com>
2025-09-06 18:32:42 +0300

    Add: Authorization and endpoints to ApplicationsController for submitting and searching seller applications

--- API/Controllers/ApplicationsController.cs
+++ API/Controllers/ApplicationsController.cs
@@ -1,8 +1,46 @@
+using Application.Common.Interfaces;
+using Application.DTOs.store;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Domain.Constants;
 
 namespace API.Controllers;
-[Route("api/[controller]")]
+
 [ApiController]
-public class ApplicationsController :ControllerBase
+[Route("api/[controller]")]
+[Authorize]
+public class ApplicationsController(IApplicationsService applicationsService) : ControllerBase
 {
+    [HttpPost()]
+    [Authorize(Roles = AuthConstants.Roles.User)]
+    public async Task<IActionResult> SubmitApplication([FromBody] SellerApplicationDto dto)
+    {
+        var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value!);
+        var result = await applicationsService.SubmitApplicationAsync(userId, dto);
+        
+        if (result.Success)
+        {
+            return Ok(result);
+        }
+        
+        return BadRequest(result);
+    }
+    
+    [HttpGet()]
+    [Authorize(Roles = AuthConstants.Roles.Admin)]
+    public async Task<IActionResult> SearchSellerApplications(
+        [FromQuery] int page = 1,
+        [FromQuery] int limit = 25,
+        [FromQuery] string? status = null,
+        CancellationToken cancellationToken = default)
+    {
+        var response = await applicationsService.SearchSellerApplicationsAsync(
+            page, 
+            limit, 
+            status, 
+            cancellationToken);
+        
+        return Ok(response);
+    }
 }
