35b04a4a1de3db8cf340a35efeb658e46b276451
Ozzy-ZY <zyadhimself1@gmail.com>
2025-09-08 01:04:27 +0300

    Update: Switch default connection string to ProductionDatabase and extend appsettings with email, Kinguin, and photo options

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -27,7 +27,7 @@ public static class DependencyInjection
         services.Configure<KinguinSettings>(configuration.GetSection(KinguinSettings.SectionName));
         
         services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(configuration.GetConnectionString("ProductionDatabase")));
         
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddHostedService<DataBaseSeederService>();
