e87a318b6592f6968eb34372af57214525ed2142
Ozzy-ZY <zyadhimself1@gmail.com>
2025-09-07 19:31:05 +0300

    Fix: Correct route templates in RoleController endpoints for AddSeller and RemoveSeller

--- API/Controllers/RoleController.cs
+++ API/Controllers/RoleController.cs
@@ -9,7 +9,7 @@ namespace API.Controllers;
 [Authorize(Roles = AuthConstants.Roles.Admin)]
 public class RoleController(IRoleService roleService) : ControllerBase
 {
-    [HttpPost("Add-Seller{userId:int}")]
+    [HttpPost("Add-Seller/{userId:int}")]
     public async Task<IActionResult> AddSeller([FromRoute]int userId)
     {
         var result = await roleService.AddSellerAsync(userId);
@@ -20,7 +20,7 @@ public class RoleController(IRoleService roleService) : ControllerBase
         return Ok(result);
     }
 
-    [HttpDelete("Remove-Seller{userId:int}")]
+    [HttpDelete("Remove-Seller/{userId:int}")]
     public async Task<IActionResult> RemoveSeller([FromRoute] int userId)
     {
         var result = await roleService.RemoveSellerRole(userId);
